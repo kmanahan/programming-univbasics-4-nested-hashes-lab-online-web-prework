5ab94ffe04ee1fbfbe5abc9d552e491dfd86bc43
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-09-29 17:45:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
     programmer_hash[:alan_kay][:known_for] = "GUI"
-    puts [:alan_kay][:known_for]
 end
 
 def adding_to_dennis
